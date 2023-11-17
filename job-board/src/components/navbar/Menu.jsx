4317e6e452682489f72749c7e0e8eaf82609c404
Gabriel <geraldg652@gmail.com>
2023-11-17 11:05:34 +0300

    navbar completed

--- job-board/src/components/navbar/Menu.jsx
+++ job-board/src/components/navbar/Menu.jsx
@@ -1,6 +1,6 @@
-export default function Menu({ children }) {
+export default function Menu({ children, isMenuOpen }) {
     return (
-        <nav className="text-xl hidden md:flex">
+        <nav className={`text-xl ${isMenuOpen ? 'block' : 'hidden'} md:flex transition-all ease-in duration-500`}>
             <ul className="md:flex md:items-center md:space-x-3">
                 {children}
             </ul>
