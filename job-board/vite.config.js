ee0493348584ac1c7eacee7503cc6ee299171a25
Gabriel <geraldg652@gmail.com>
2023-12-18 14:29:46 +0300

    fix: links

--- job-board/vite.config.js
+++ job-board/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api/v1': {
-        target: 'http://127.0.0.1:8080',
-        // target: 'https://job-board-gk5p.onrender.com',
+        // target: 'http://127.0.0.1:8080',
+        target: 'https://job-board-gk5p.onrender.com',
         changeOrigin: true
       }
     }
