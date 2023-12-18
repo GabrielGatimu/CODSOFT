ed1a62ff5dc99751a45f41532574d87771a31456
Gabriel <geraldg652@gmail.com>
2023-12-18 08:10:26 +0300

    fix: CORS

--- job-board/src/state/slices/api.slice.js
+++ job-board/src/state/slices/api.slice.js
@@ -1,6 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
+    // baseUrl: "http://127.0.0.1:8080/api/v1",
     baseUrl: "https://job-board-gk5p.onrender.com/api/v1",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
--- job-board/vite.config.js
+++ job-board/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/job-board/",
   server: {
     port: 5173,
     proxy: {
       '/api/v1': {
+        // target: 'http://127.0.0.1:8080',
         target: 'https://job-board-gk5p.onrender.com',
         changeOrigin: true
       }
